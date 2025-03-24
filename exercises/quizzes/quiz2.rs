4e3573f744593e2d245a6d4b72c040ca94f769e5
lcscosta <lcscosta@disroot.org>
2025-03-24 17:13:22 -0300

    Added solutions from 40 to 46

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,19 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> { 
+        let mut output = Vec::new();
+        for (word, command) in input {
+            let transformed = match command {
+                Command::Uppercase => word.to_uppercase(),
+                Command::Trim => word.trim().to_string(),
+                Command::Append(count) => format!("{}{}", word, "bar".repeat(count)),
+            };
+            output.push(transformed);
+        }
+        
+        output
+    }
 }
 
 fn main() {
@@ -37,7 +49,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
